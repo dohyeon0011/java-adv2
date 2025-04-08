a39af24ba54c875157bea3997d6c679376c1d7e7
idohyeon <ehgus5026@naver.com>
2025-04-08 17:36:32 +0900

    -파일 쓰기: 한 번에 파일 크기 만큼 쓰기

---
+++ src/io/buffered/CreateFileV4.java
@@ -0,0 +1,27 @@
+package io.buffered;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import static io.buffered.BufferedConst.FILE_NAME;
+import static io.buffered.BufferedConst.FILE_SIZE;
+
+public class CreateFileV4 {
+
+    public static void main(String[] args) throws IOException {
+        FileOutputStream fos = new FileOutputStream(FILE_NAME);
+        long startTime = System.currentTimeMillis();
+
+        byte[] buffer = new byte[FILE_SIZE]; // 10MB 한 번에 쓰기(디스크나 파일 시스템에서 데이터를 읽고 쓰는 기본 단위가 보통 4KB 또는 8KB이기 때문에, 한 번에 쓴다고 해서 무작정 빠른 것이 아님.)
+        for (int i = 0; i < FILE_SIZE; i++) {
+            buffer[i] = 1;
+        }
+        fos.write(buffer);
+        fos.close();
+
+        long endTime = System.currentTimeMillis();
+        System.out.println("File created: " + FILE_NAME);
+        System.out.println("File size: " + FILE_SIZE / 1024 / 1024 + "MB");
+        System.out.println("Time taken: " + (endTime - startTime) + "ms");
+    }
+}
