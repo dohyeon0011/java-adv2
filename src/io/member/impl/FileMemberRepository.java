97ba469370cab96b519f9793e2ce9254521f99c4
idohyeon <ehgus5026@naver.com>
2025-04-09 18:33:42 +0900

    -회원 정보 파일 저장

---
+++ src/io/member/impl/FileMemberRepository.java
@@ -0,0 +1,47 @@
+package io.member.impl;
+
+import io.member.Member;
+import io.member.MemberRepository;
+
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
+
+import static java.nio.charset.StandardCharsets.*;
+
+public class FileMemberRepository implements MemberRepository {
+
+    private static final String FILE_PATH = "temp/members-txt.dat";
+    private static final String DELIMITER = ",";
+
+    @Override
+    public void add(Member member) {
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(FILE_PATH, UTF_8, true))) {
+            bw.write(member.getId() + DELIMITER + member.getName() + DELIMITER + member.getAge());
+            bw.newLine(); // 다음 라인으로 넘기기
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public List<Member> findAll() {
+        List<Member> members = new ArrayList<>();
+
+        try (BufferedReader br = new BufferedReader(new FileReader(FILE_PATH, UTF_8))) {
+            String line;
+
+            while ((line = br.readLine()) != null) {
+                String[] memberData = line.split(DELIMITER);
+                members.add(new Member(memberData[0], memberData[1], Integer.parseInt(memberData[2])));
+            }
+
+            return members;
+        } catch (FileNotFoundException e) {
+            return new ArrayList<>();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
