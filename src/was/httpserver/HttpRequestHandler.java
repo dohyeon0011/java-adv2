1ebc9494b4e6dee6f4b6bb0de58cdd8466c1b58c
idohyeon <ehgus5026@naver.com>
2025-04-15 20:03:33 +0900

    -HttpRequestHandler(커맨드 패턴)

---
+++ src/was/httpserver/HttpRequestHandler.java
@@ -0,0 +1,90 @@
+package was.httpserver;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+
+import static util.MyLogger.log;
+
+public class HttpRequestHandler implements Runnable {
+    private final Socket socket;
+    private final ServletManager servletManager;
+
+    public HttpRequestHandler(Socket socket, ServletManager servletManager) {
+        this.socket = socket;
+        this.servletManager = servletManager;
+    }
+
+    @Override
+    public void run() {
+        try {
+            process();
+        } catch (Exception e) {
+            log(e);
+        }
+    }
+
+    private void process() throws IOException {
+        try (socket;
+             BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream(), StandardCharsets.UTF_8));
+             PrintWriter printWriter = new PrintWriter(socket.getOutputStream(), false, StandardCharsets.UTF_8)) {
+
+            HttpRequest request = new HttpRequest(reader);
+            HttpResponse response = new HttpResponse(printWriter);
+
+            log("HTTP 요청: " + request);
+            servletManager.excute(request, response);
+            response.flush();
+            log("HTTP 응답 완료");
+        }
+    }
+
+    private static String requestsToString(BufferedReader reader) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        String line;
+
+        while ((line = reader.readLine()) != null) {
+            if (line.isEmpty()) {
+                break;
+            }
+            sb.append(line).append("\n");
+        }
+
+        return sb.toString();
+    }
+
+    private void site1(HttpResponse response) {
+        response.writeBody("<h1>site1</h1>");
+    }
+
+    private void site2(HttpResponse response) {
+        response.writeBody("<h2>site1</h2>");
+    }
+
+    private void search(HttpRequest request, HttpResponse response) {
+        String query = request.getParameter("q");
+
+        response.writeBody("<h1>Search</h1>");
+        response.writeBody("<ul>");
+        response.writeBody("<li>query: " + query + "</li>");
+        response.writeBody("</ul>");
+    }
+
+    private void home(HttpResponse response) {
+        // 원칙적으로 Content-Length를 계산해서 전달해야 하지만, 예제를 단순하게 설명하기 위해 생략(브라우저에 따라 생략해도 되고 안해도 됨)
+        response.writeBody("<h1>home</h1>");
+        response.writeBody("<ul>");
+        response.writeBody("<li><a href='/site1'>site1</a></li>");
+        response.writeBody("<li><a href='/site2'>site2</a></li>");
+        response.writeBody("<li><a href='/search?q=hello'>검색</a></li>");
+        response.writeBody("</ul");
+    }
+
+    private void notFound(HttpResponse response) {
+        response.setStatusCode(404);
+        response.writeBody("<h1>404 페이지를 찾을 수 없습니다.</h1>");
+    }
+}
