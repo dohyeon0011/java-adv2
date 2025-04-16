b043fd46f2863e6421acc997a8c05701916ab7ed
idohyeon <ehgus5026@naver.com>
2025-04-16 21:54:16 +0900

    -회원 정보 관리

--- src/was/v8/ServerMainV8.java
+++ src/was/v8/ServerMainV8.java
@@ -4,6 +4,7 @@ import was.httpserver.HttpServer;
 import was.httpserver.ServletManager;
 import was.httpserver.servlet.DiscardServlet;
 import was.httpserver.servlet.annotation.AnnotationServletV2;
+import was.httpserver.servlet.annotation.AnnotationServletV3;
 
 import java.io.IOException;
 import java.util.List;
@@ -14,7 +15,8 @@ public class ServerMainV8 {
 
     public static void main(String[] args) throws IOException {
         List<Object> controllers = List.of(new SiteControllerV8(), new SearchControllerV8());
-        AnnotationServletV2 annotationServlet = new AnnotationServletV2(controllers);
+//        AnnotationServletV2 annotationServlet = new AnnotationServletV2(controllers);
+        AnnotationServletV3 annotationServlet = new AnnotationServletV3(controllers);   // `AnnotationServletV3` 을 생성하는 시점에 `@Mapping` 을 사용하는 컨트롤러의 메서드를 모두 찾아서 `pathMap` 에 보관한다.
 
         ServletManager servletManager = new ServletManager();
         servletManager.setDefaultServlet(annotationServlet);
