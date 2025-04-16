c1c96df9986e36e3eb1d2b797e3ed4efbc0a8a93
idohyeon <ehgus5026@naver.com>
2025-04-16 19:56:26 +0900

    -커스텀 애노테이션 클래스 단위 적용

---
+++ src/annotaion/basic/ElementData2.java
@@ -0,0 +1,5 @@
+package annotaion.basic;
+
+@AnnoElement(value = "data", tags = "t1")
+public class ElementData2 {
+}
---
+++ src/annotaion/basic/ElementData3.java
@@ -0,0 +1,5 @@
+package annotaion.basic;
+
+@AnnoElement("data")    // 하나만 쓰는 경우에는 어노테이션 메서드 이름 생략 가능
+public class ElementData3 {
+}
