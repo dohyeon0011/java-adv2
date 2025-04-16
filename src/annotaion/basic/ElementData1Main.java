65a8a42ec0bfeae59daa9d6e7f9ec5bb1f69f880
idohyeon <ehgus5026@naver.com>
2025-04-16 19:58:53 +0900

    -클래스 단위로 적용된 애노테이션 확인 클래스

---
+++ src/annotaion/basic/ElementData1Main.java
@@ -0,0 +1,20 @@
+package annotaion.basic;
+
+public class ElementData1Main {
+
+    public static void main(String[] args) {
+        Class<ElementData1> annoClass = ElementData1.class;
+        AnnoElement annotation = annoClass.getAnnotation(AnnoElement.class);
+
+        String value = annotation.value();
+        System.out.println("value = " + value);
+
+        int count = annotation.count();
+        System.out.println("count = " + count);
+
+        String[] tags = annotation.tags();
+        for (String tag : tags) {
+            System.out.println("tag = " + tag);
+        }
+    }
+}
